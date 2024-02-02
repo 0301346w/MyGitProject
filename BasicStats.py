5026ce7cd87f1e58b656260009fbdeef9dbafc56
0301346w <157614209+0301346w@users.noreply.github.com>
2024-02-02 15:00:54 -0400

    Update BasicStats.py
    Added average function

--- BasicStats.py
+++ BasicStats.py
@@ -1,11 +1,17 @@
 def find_sum(numbers):
     return sum(numbers)
  
-
+def find_average(numbers):
+    if numbers:
+        return sum(numbers) / len(numbers)
+    else:
+        return "List is empty, cannot calculate average."
  
-def display_results(sum_result):
+ 
+ 
+def display_results(sum_result, average_result):
     print(f"Sum of the numbers: {sum_result}")
-
+    print(f"Average of the numbers: {average_result}")
  
 def main():
     # Get user input for a list of numbers
@@ -14,10 +20,10 @@ def main():
  
     # Perform list operations
     sum_result = find_sum(user_numbers)
-
+    average_result = find_average(user_numbers)
  
     # Display the results
-    display_results(sum_result)
+    display_results(sum_result, average_result)
  
 if __name__ == "__main__":
     main()
